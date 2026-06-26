8895cc956e93cab4dff697cec156963489539ca7
ELOI-CODE1 <izabayoeloi2@gmail.com>
2026-06-26 15:12:17 +0200

    fix:add vercel speed insight

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
 import { inject } from '@vercel/analytics';
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 // Initialize Vercel Analytics
 inject();
